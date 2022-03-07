c5f819a6b35549a146beeaec8f86b670e1d5ef9f
SonYoonSeok <dbstjr5517@naver.com>
2022-03-07 20:07:02 +0900

    ⚡️ :: (#1) UserService

--- src/main/java/bigdater/dater/dto/request/SignUpUserRequest.java
+++ src/main/java/bigdater/dater/dto/request/SignUpUserRequest.java
@@ -1,4 +1,44 @@
 package bigdater.dater.dto.request;
 
-public class SignUpRequest {
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SignUpUserRequest {
+
+    @NotEmpty(message = "이메일을 입력해주세요.")
+    @Email
+    private String email;
+
+    @NotEmpty(message = "비밀번호를 입력해주세요.")
+    private String password;
+
+    @NotEmpty(message = "닉네임을 입력해주세요.")
+    private String nickname;
+
+    @NotNull(message = "성별을 선택해주세요.")
+    private Boolean sex;
+
+    @NotNull(message = "안밖을 선택해주세요.")
+    private Boolean isInsider;
+
+    @NotEmpty(message = "이메일을 입력해주세요.")
+    private String freeTime;
+
+    @NotEmpty(message = "동반자를 입력해주세요.")
+    private String companion;
+
+    @NotEmpty(message = "음식 종류를 입력해주세요.")
+    private String food;
+
+    @NotEmpty(message = "연령대를 선택해주세요.")
+    private String age;
 }
--- src/main/java/bigdater/dater/service/UserService.java
+++ src/main/java/bigdater/dater/service/UserService.java
@@ -1,2 +1,44 @@
-package bigdater.dater.service;public class UserService {
+package bigdater.dater.service;
+
+import bigdater.dater.dto.request.SignUpUserRequest;
+import bigdater.dater.entity.user.User;
+import bigdater.dater.entity.user.UserRepository;
+import bigdater.dater.entity.user.survey.Survey;
+import bigdater.dater.entity.user.survey.SurveyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final SurveyRepository surveyRepository;
+
+    @Transactional
+    public ResponseEntity save(SignUpUserRequest request) {
+        User savedUser = userRepository.save(User.builder()
+                //.email(request.getEmail())
+                .password(request.getPassword())
+                .build());
+        surveySave(request, savedUser);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @Transactional
+    private void surveySave(SignUpUserRequest request, User user) {
+        surveyRepository.save(Survey.builder()
+                .nickname(request.getNickname())
+                .sex(request.getSex())
+                .isInsider(request.getIsInsider())
+                .freeTime(request.getFreeTime())
+                .companion(request.getCompanion())
+                .food(request.getFood())
+                .age(request.getAge())
+                .user(user)
+                .build());
+    }
 }
